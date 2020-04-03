7f8d74d926238369123ceaf0b3471a329e1630b8
Serg Klukvin <klukvin@ukr.net>
2020-04-03 22:44:13 +0300

    finish

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let arr = ["яблуко", "мандарин", "апельсин", "лимон",
 
 document.write(`<h4>Початковий масив фруктів:</h4> ${arr}<br>`);
 
-let arrNew = arr.slice();
+arrNew = arr.slice();
 arrNew.sort();
 
 document.write(`<br> <br> <br><h4>Відсортований масив фруктів:</h4>`);
@@ -13,7 +13,7 @@ document.write(`</ul>`);
 function listArr(arrNew) {
   let tmp;
   for (let i = 0; i < arrNew.length; i++) {
-    tmp += document.write(`<li>${arr[i]}</li><br>`);
+    tmp += document.write(`<li>${arrNew[i]}</li><br>`);
   }
   return tmp;
 }
